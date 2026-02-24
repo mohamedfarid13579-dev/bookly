25d5351cda00c274ca89043136b8b93622249516
mohamed-farid <59448168+mohamed-farid@users.noreply.github.com>
2026-02-24 17:13:12 +0200

    file costuction and start to make splash

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: SplashView());
+  }
+}
