4e6542f88fadc67b8088429b9368a16af8d114a3
mohamed-farid <59448168+mohamed-farid@users.noreply.github.com>
2026-02-27 22:07:07 +0200

    create app sytyles

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/home/presentation/views/widgets/cutome_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_list_view_Item.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,35 @@ class HomeViewBody extends StatelessWidget {
           CustomeAppBar(),
           FeaturedBooksListView(),
           SizedBox(height: 40),
-          Text("Best Seller", style: Styles.titleMediam),
+          Text("Best Seller", style: Styles.textStyle18),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.book1),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/constance.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
@@ -5,4 +6,30 @@ abstract class Styles {
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGTSectraFine,
+  );
+   static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGTSectraFine,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  
+ 
 }
