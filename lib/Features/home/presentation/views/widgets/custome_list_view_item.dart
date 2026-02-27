616b98c48befc65b257b200c6840d9d34bbcfc49
mohamed-farid <59448168+mohamed-farid@users.noreply.github.com>
2026-02-27 14:18:25 +0200

    add featured list view

--- lib/Features/home/presentation/views/widgets/custome_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custome_list_view_item.dart
@@ -1,22 +1,19 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomeListViewItem extends StatelessWidget {
-  const CustomeListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.book1),
-            ),
+    return AspectRatio(
+      aspectRatio: 2.4 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.book1),
           ),
         ),
       ),
