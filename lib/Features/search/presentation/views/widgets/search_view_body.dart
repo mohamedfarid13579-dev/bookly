340d8cdc31098da530bce30905ead6fd7005cdaa
mohamed-farid <59448168+mohamed-farid@users.noreply.github.com>
2026-03-07 00:15:38 +0200

    search View Done

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly/Features/search/presentation/views/widgets/cutome_search_text_field.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -8,7 +10,33 @@ class SearchViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [CutomeSearchTextField()]),
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          CutomeSearchTextField(),
+          SizedBox(height: 20),
+          Text("Search Results", style: Styles.textStyle18),
+          SizedBox(height: 20),
+          Expanded(child: SearchResultSearchView()),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchResultSearchView extends StatelessWidget {
+  const SearchResultSearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(),
+        );
+      },
     );
   }
 }
